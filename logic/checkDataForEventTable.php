08ce1576cd64e67d3bebc810f10763f97ada2070
Velly <breshchinskaya@mail.ru>
2019-10-20 23:54:39 +0300

    Скрипт обработки отправленных данных из формы добавления эвента

---
+++ logic/checkDataForEventTable.php
@@ -0,0 +1,13 @@
+<?php
+include "dbLogic/workWithDB.php";
+session_start();
+if (isset($_POST['name'])) $name=$_POST['name'];
+if (isset($_POST['date'])) $date=$_POST['date'];
+if (isset($_POST['city'])) $city=$_POST['city'];
+if (isset($_POST['participants'])) $participants=$_POST['participants'];
+if(isset($_SESSION['user'])) $master=$_SESSION['user'];
+$allowed = array("name","date","city", "master", "participants");
+$values = array($name, $date, $city, $master, $participants);
+writeNewEvent($allowed, $values);
+header("Location: ../secret.php");
+?>
