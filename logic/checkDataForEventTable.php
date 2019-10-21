70662acd29d8f662c83a524b29ec2c74fba43f83
Velly <breshchinskaya@mail.ru>
2019-10-21 17:10:31 +0300

    Забыла инициировать вне if

--- logic/checkDataForEventTable.php
+++ logic/checkDataForEventTable.php
@@ -1,6 +1,7 @@
 <?php
 include "dbLogic/workWithDB.php";
 session_start();
+$name=""; $date=""; $city=""; $master=""; $participants="";
 if (isset($_POST['name'])) $name=$_POST['name'];
 if (isset($_POST['date'])) $date=$_POST['date'];
 if (isset($_POST['city'])) $city=$_POST['city'];
