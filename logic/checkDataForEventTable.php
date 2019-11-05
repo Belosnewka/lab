589994725ae6a2550476ff66743a3c6994ea23b7
Velly <breshchinskaya@mail.ru>
2019-11-05 12:34:25 +0300

    добавлена проверка на наличие города в бд при добавлении события

--- logic/checkDataForEventTable.php
+++ logic/checkDataForEventTable.php
@@ -7,8 +7,26 @@ if (isset($_POST['date'])) $date=$_POST['date'];
 if (isset($_POST['city'])) $city=$_POST['city'];
 if (isset($_POST['participants'])) $participants=$_POST['participants'];
 if(isset($_SESSION['user'])) $master=$_SESSION['user'];
-$allowed = array("name","date","city", "master", "participants");
-$values = array($name, $date, $city, $master, $participants);
-writeNewEvent($allowed, $values);
-header("Location: ../secret.php");
+$res=askAllCitiesFromBD();
+$checkCity=0;
+foreach ($res as $row)
+{
+  if($row['city']===$city)
+  {
+    $checkCity=$row['id'];
+    break;
+  }
+}
+if($checkCity>0)
+{
+  $allowed = array("name","date","city", "master", "participants");
+  $values = array($name, $date, $checkCity, $master, $participants);
+  writeNewEvent($allowed, $values);
+  header("Location: ../secret.php");
+}
+else
+{
+  $mes='Нет такого города!';
+  header("Location: ../errorPage.php?mes=$mes");
+}
 ?>
